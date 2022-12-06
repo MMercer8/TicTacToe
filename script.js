fc26f721f94f49a3722c5d7a3fb14daa757e5eab
Michael Mercer <michaelmercer89@gmail.com>
2022-12-06 18:24:29 -0500

    initial  file save

---
+++ script.js
@@ -0,0 +1,38 @@
+// Store the gameboard as an array inside of a gameboard OBJECT. == START HERE
+//gameboard == module
+
+/** 
+ * 
+ * gameObject
+ *  function addX
+ * function addY
+ * 
+ * 
+ */
+var gameBoard = {
+    //gameboard
+    
+}
+//Players also to be stored in objects
+// players = factory?
+
+// Prob want object to control the flow of the game itself
+//  controller = module
+/** 
+ * 
+ * gameObject
+ *  function win
+ * function lose
+ * 
+ * 
+ */
+
+/*
+
+ Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory. 
+ 
+ ****Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module. 
+ 
+ ***If you need multiples of something (players!), create them with factories.
+
+ */
