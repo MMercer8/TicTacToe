6f0525728874a6754354e374f5042275e94f0195
Michael Mercer <michaelmercer89@gmail.com>
2022-12-07 08:59:04 -0500

    update 1

--- script.js
+++ script.js
@@ -9,10 +9,27 @@
  * 
  * 
  */
-var gameBoard = {
-    //gameboard
+// var gameBoard = {
+//     //gameboard
     
-}
+// }
+
+//+++++++++++++++ IIFE +++++++++++++++
+let gameBoard = (function() {
+    console.log("gameboard");
+    let gameBoard = [
+        null, null, null,
+        null, null, null,
+        null, null, null];
+     console.table(gameBoard);
+     let addX = function () {
+        console.log("add x");
+     };
+     let addO = function() {
+        console.log("O its OH!");
+     }
+     return {addX, addO, gameBoard};
+}) ();
 //Players also to be stored in objects
 // players = factory?
 
